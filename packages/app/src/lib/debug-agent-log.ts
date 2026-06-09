0c6caa4240ead57f850c4f025fc2806703c401bf
Min Chun Fu <itsnotaka@Gmail.com>
2026-06-09 14:13:26 -0400

    Log agent debug events to console

--- packages/app/src/lib/debug-agent-log.ts
+++ packages/app/src/lib/debug-agent-log.ts
@@ -5,6 +5,8 @@ export function debugAgentLog(
   hypothesisId: string,
 ): void {
   // #region agent log
+  // eslint-disable-next-line no-console
+  console.log(`[agent-debug][${hypothesisId}] ${message}`, { location, ...data });
   fetch("http://127.0.0.1:7811/ingest/daa174af-74be-48e1-9656-4b50f1e7b673", {
     method: "POST",
     headers: {
